0ad4f225845fa606b70b2dd9c8f295ca93d2825a
Abeeb <you@example.com>
2023-09-09 12:16:36 +0100

    new changes

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-const nowDate2 = Date.now()
-const time = document.getElementById('time')
-time.innerText = nowDate2
-
-const date = new Date('September 10, 2023 01:15:00')
-const today = date.getDay() + 1
+function changing() {
+  const day = Date.now()
+  const time = document.getElementById('time')
+  time.innerText = day
+}
+changing()
+setInterval(changing, 1)
+const date = new Date()
+const today = date.getDay()
+console.log(today)
 const arr = [
-  {
-    id: 1,
-    day: 'Sunday',
-  },
-  {
-    id: 2,
-    day: 'Monday',
-  },
-  {
-    id: 3,
-    day: 'Tuesday',
-  },
-  {
-    id: 4,
-    day: 'Wednesday',
-  },
-  {
-    id: 5,
-    day: 'Thursday',
-  },
-  {
-    id: 6,
-    day: 'Friday',
-  },
-  {
-    id: 7,
-    day: 'Saturday',
-  },
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ]
 
-const nn = arr.filter(({ id, day }) => {
-  return id == today
-})
-
-const nnn = nn[0]
-
 const day = document.getElementById('day')
-day.innerText = nnn.day
+day.innerText = arr[today]
