0a56f271f0a5f4b61775b59106ba29203956a4bc
abeebdon <abeebdon@gmail.com>
2025-02-14 12:20:46 +0100

    Stage three loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Step1 from './components/Step1'
 
 const App = () => {
+  const [stepNumber, setStepNumber] = useState(1)
   return (
     <main className="h-screen py-6 px-6 lg:px-10 ">
       <Header />
+      <Step1 />
     </main>
   )
 }
---
+++ src/components/Step1.jsx
@@ -0,0 +1,23 @@
+const Step1 = () => {
+  return (
+    <section className="mt-12 w-full max-w-[700px] mx-auto border rounded-4xl border-[#0E464F] p-12">
+      <div className="flex items-center justify-between text-white">
+        <h2>Ticket Selection</h2>
+        <div>Step1/3</div>
+      </div>
+      <div className="mt-8 p-6 border">
+        <h2>Techember Fest "25</h2>
+        <p>
+          Join us for an unforgettable experience at [Event Name]! Secure your
+          spot now.
+        </p>
+        <div>
+          <p>📍 [Event Location]</p>
+          <p>March 15, 2025 | 7:00 PM</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Step1
